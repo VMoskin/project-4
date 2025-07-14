e56679aa3306a5d118d2d227f543dab81ace1efa
viktor moskin <vik52322@gmail.com>
2025-07-14 17:13:45 +0300

    import function from masks

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,11 @@
+import masks
+
 def mask_account_card(num_card: str) -> str:
     """функция принимает номер карты или счета и возвращает маску номера"""
     if "Счет" in num_card:
-        return f"{num_card[0:5]} **{num_card[-4:]}"
+        return f"{num_card[0:4]} {masks.get_mask_account(num_card[-20:])}"
     else:
-        return f"{num_card[:-16]}{num_card[-16:-12]} {num_card[-12:-10]}** **** {num_card[-4:]}"
+        return f"{num_card[:-16]}{masks.get_mask_card_number(num_card[-16:])}"
 
 
 def get_date(date_string: str) -> str:
@@ -11,7 +13,7 @@ def get_date(date_string: str) -> str:
     return f"{date_string[8:10]}.{date_string[5:7]}.{date_string[:4]}"
 
 
-print(mask_account_card("Счет 12345678987654326545"))
-print(mask_account_card("Maestro 7000543267892587"))
-print(mask_account_card("Visa Platinum 7000321065419874"))
-print(get_date("2024-03-11T02:26:18.671407"))
+print(mask_account_card("Счет 92345678987654326541"))
+print(mask_account_card("Maestro 7000543267892582"))
+print(mask_account_card("Visa Platinum 7000321065419873"))
+print(get_date("2024-03-12T02:26:18.671407"))
