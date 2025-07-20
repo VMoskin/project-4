840cab807aedb1e9fe2951b92b44b98df09c79c2
viktor moskin <vik52322@gmail.com>
2025-07-20 15:10:45 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
-def filter_by_state(transactions, state='EXECUTED'):
+def filter_by_state(transactions: list, state='EXECUTED') ->list:
+
     new_transactions = []
     for transaction in transactions:
         for value in transaction.values():
@@ -6,4 +7,11 @@ def filter_by_state(transactions, state='EXECUTED'):
                 new_transactions.append(transaction)
     return new_transactions
 
-№filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+
+def sort_by_date(list_transaction: list, reverse=True):
+
+    sorted_list = sorted(list_transaction, key=lambda x: x['date'], reverse=(reverse))
+    #return sorted_list
+    print(sorted_list)
+filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], reverse=False)
