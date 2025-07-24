feee8f0bf80d7e75d9f96824368064787327cd99
viktor moskin <vik52322@gmail.com>
2025-07-24 18:24:55 +0300

    error correction

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(transactions: list, state='EXECUTED') -> list:
+def filter_by_state(transactions: list, state: str="EXECUTED") -> list:
     """Функция сортирует список словарей по значению ключа 'state'"""
     new_transactions = []
     for transaction in transactions:
@@ -8,11 +8,30 @@ def filter_by_state(transactions: list, state='EXECUTED') -> list:
     return new_transactions
 
 
-def sort_by_date(list_transaction: list, sort_rev=True):
+def sort_by_date(list_transaction: list, sort_rev: bool=True) -> list:
     """Функция сортирует список словарей по дате"""
-    sorted_list = sorted(list_transaction, key=lambda x: x['date'], reverse=(sort_rev))
+    sorted_list = sorted(list_transaction, key=lambda x: x["date"], reverse=(sort_rev))
     return sorted_list
 
+
 if __name__ == "__main__":
-    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
+    print(
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
--- src/widget.py
+++ src/widget.py
@@ -13,6 +13,7 @@ def get_date(date_string: str) -> str:
     """Фунция возвращает дату в другом формате"""
     return f"{date_string[8:10]}.{date_string[5:7]}.{date_string[:4]}"
 
+
 if __name__ == "__main__":
     print(mask_account_card("Счет 92345678987654326541"))
     print(mask_account_card("Maestro 7000543267892582"))
