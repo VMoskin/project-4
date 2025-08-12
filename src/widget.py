dda14f4343dc55d6128d32b856fa19cfd77bad64
viktor moskin <vik52322@gmail.com>
2025-08-12 21:59:49 +0300

    add test_widget

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,22 @@
-import masks
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
 
 def mask_account_card(num_card: str) -> str:
     """функция принимает номер карты или счета и возвращает маску номера"""
     if "Счет" in num_card:
-        return f"{num_card[0:4]} {masks.get_mask_account(int(num_card[-20:]))}"
+        if len(num_card) == 25 and num_card[-20:].isdigit():
+            return f"{num_card[0:4]} {get_mask_account(int(num_card[-20:]))}"
+        else:
+            return "Неверный номер счета"
     else:
-        return f"{num_card[:-16]}{masks.get_mask_card_number(int(num_card[-16:]))}"
+        if len(num_card) > 16:
+            if num_card[-17] == ' ' and num_card[-16:].isdigit():
+                return f"{num_card[:-16]}{get_mask_card_number(int(num_card[-16:]))}"
+            else:
+                return "Неверный номер карты"
+        else:
+            return "Неверный номер карты"
 
 
 def get_date(date_string: str) -> str:
@@ -15,7 +25,8 @@ def get_date(date_string: str) -> str:
 
 
 if __name__ == "__main__":
-    print(mask_account_card("Счет 92345678987654326541"))
+    print(mask_account_card("Счет 92345678987654326543l"))
     print(mask_account_card("Maestro 7000543267892582"))
-    print(mask_account_card("Visa Platinum 7000321065419873"))
+    print(mask_account_card("Maestro"))
+    print(mask_account_card("Visa Platinum 700032106541987"))
     print(get_date("2024-03-12T02:26:18.671407"))
