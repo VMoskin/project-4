6057fe382cfbc788fcfff6b7fcf35b81b7bc105b
viktor moskin <vik52322@gmail.com>
2025-08-12 09:19:22 +0300

    add test for masks

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,21 @@
 def get_mask_card_number(card_number_1: int) -> str:
     """Функция принимает на вход номер карты и возвращает маску номера"""
     card_number = str(card_number_1)
-    if len(card_number) == 16:
+    if len(card_number) == 16 and card_number.isdigit():
         return f"{card_number[:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
     else:
-        return 'Неверный номер карты'
+        return "Неверный номер карты"
 
 
 def get_mask_account(account_number_1: int) -> str:
     """Функция принимает на вход номер счета и возвращает маску номера"""
     account_number = str(account_number_1)
-    if len(account_number) == 20:
+    if len(account_number) == 20 and account_number.isdigit():
         return f"**{account_number[-4:]}"
     else:
-        return 'Неверный номер счета'
+        return "Неверный номер счета"
 
 
 if __name__ == "__main__":
-    #print(get_mask_card_number(1234567898765411))
-    #print(get_mask_account(24681357984629753155))
-    print(get_mask_card_number(''))
+    print(get_mask_card_number(1234567898765417))
+    print(get_mask_account(24681357984629753155))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,12 +1,26 @@
+import pytest
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number(1234567898765432) == '1234 56** **** 5432'
-    assert get_mask_card_number(4567898765433) == 'Неверный номер карты'
-    assert get_mask_card_number(1111234567898765432) == 'Неверный номер карты'
-    assert get_mask_card_number('') == 'Неверный номер карты'
-    assert get_mask_account(11111234567898765432) == '**5432'
-    assert get_mask_account(1111234567898765432) == 'Неверный номер счета'
-    assert get_mask_account('') == 'Неверный номер счета'
+@pytest.mark.parametrize('number_card, value_card', [
+    ('1111222233334444', "1111 22** **** 4444"),
+    ('111122223333444', "Неверный номер карты"),
+    ('1111222O33334444', "Неверный номер карты"),
+    ('11112222333344445', "Неверный номер карты"),
+    ('', "Неверный номер карты"),
+])
+def test_get_mask_card_number(number_card: int, value_card: str) -> None:
+    assert get_mask_card_number(number_card) == value_card
+
+
+@pytest.mark.parametrize('name_account, value_account', [
+    ('11112222333344445555', "**5555"),
+    ('1111222233334444555', "Неверный номер счета"),
+    ('111122223333444455576', "Неверный номер счета"),
+    ('1111222233334444555O', "Неверный номер счета"),
+    ('1111222233334444555o', "Неверный номер счета"),
+    ('', "Неверный номер счета")
+])
+def test_get_mask_account(name_account: int, value_account: str) -> None:
+    assert get_mask_account(name_account) == value_account
